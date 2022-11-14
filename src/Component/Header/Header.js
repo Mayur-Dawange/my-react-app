36de1caee586afc9d7d1ed83102be0617abceffc
Mayur <mdawange@techstitcher.com>
2022-11-14 14:35:24 +0530

    added scss and uninstall

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import React from 'react';
+import css from './Header.css';
+// import { Container, Nav, Navbar } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
   return (
     <>
-    <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" className='text-white' >React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto text-white">
+            <Nav.Link href="#home" className='text-white'>Home</Nav.Link>
+            <Nav.Link href="#link" className='text-white'>Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown nav-links">
+              <NavDropdown.Item  href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              {/* <NavDropdown.Divider /> */}
+              {/* <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item> */}
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   )
 }
 
-export default Header
+export default Header;
