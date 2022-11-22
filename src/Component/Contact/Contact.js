6e54b516e8d0647b45e240bf36be2ae2a0fa1c9e
Mayur <mdawange@techstitcher.com>
2022-11-22 17:50:23 +0530

    merge into main from contact form

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -3,6 +3,8 @@ import { Container } from "react-bootstrap";
 import Faq from "../Faq/Faq";
 import Hero from "../Hero/Hero";
 import background from "../../assets/img/food2.jpg";
+import ContactFrom from "../ContactForm/ContactForm";
+import SlickItem from "../SlickItem/SlickItem";
 
 const Contact = () => {
   return (
@@ -13,14 +15,15 @@ const Contact = () => {
         pageinfo="Contact Details"
         background={background}
       />
+
+      <Container>
+        <ContactFrom />
+      </Container>
       <Faq image="https://www.superoffice.com/globalassets/blog/2016/10/how-to-build-faq-section.jpg" />
       <Container>
-        You can find us here:
-        <br />
-        React Contact
-        <br />
-        5th & 6th Floor, Royal Kapsons, A- 118, <br />
-        Sector- 136, Noida, Delhi (201305)
+        <div className="col-lg-12">
+          <SlickItem />
+        </div>
       </Container>
     </address>
   );
